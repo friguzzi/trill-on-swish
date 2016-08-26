130d9af8836483d541102000a771705c6ab23ee7
rzese <zsercr@unife.it>
2016-08-26 09:23:50 +0200

    Fixed an error that does not allowed to search in trill's examples

--- lib/examples.pl
+++ lib/examples.pl
@@ -55,6 +55,7 @@ two sources:
 
 % make example(File) find the example data
 user:file_search_path(example, swish(examples)).
+user:file_search_path(example, swish(examples/trill)).
 % make SWISH serve /example/File as example(File).
 swish_config:source_alias(example, [access(read), search('*.{pl,swinb}')]).
 
